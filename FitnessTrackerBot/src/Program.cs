ba8a42c187d824c673dd03c7fba98a30683ed758
mbo17210 <mbo17210@gmail.com>
2025-01-20 19:48:22 -0600

    Add discord token to appsettings.json

--- FitnessTrackerBot/src/Program.cs
+++ FitnessTrackerBot/src/Program.cs
@@ -1,7 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using DSharpPlus;
 using DSharpPlus.Commands;
+using Microsoft.Extensions.Configuration;
 using FitnessTrackerBot.Commands;
+using System;
 
 namespace FitnessTrackerBot;
 
@@ -9,8 +11,14 @@ class Program
 {
     static async Task Main(string[] args)
     {
+        IConfigurationRoot configurationBuilder = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        string discordKey = configurationBuilder["Discord:Token"] ?? throw new Exception("Token isn't in config. Make an appsettings.json and add it in the appropriate location.");
+
         DiscordClientBuilder builder = DiscordClientBuilder.CreateDefault(
-            Environment.GetEnvironmentVariable("FITNESS_TRACKER_TOKEN"),
+            discordKey,
             DiscordIntents.AllUnprivileged
         );
 
